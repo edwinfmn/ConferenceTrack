ee489fd47a32e7656925a240fd450a5b3a251c42
edwin.martinez <EdwinSeb2014>
2021-01-08 14:32:49 -0500

    Compiler README update

---
+++ src/main/java/Main.java
@@ -0,0 +1,42 @@
+import emartinez.conference.config.ConferenceConfig;
+import emartinez.conference.exception.ConferenceOriginException;
+import emartinez.conference.management.ConferenceManagement;
+import emartinez.conference.model.Talk;
+import emartinez.conference.util.ConferenceUtil;
+
+import java.io.FileNotFoundException;
+import java.util.List;
+
+/**
+ * Main class to execute rad Talks file and print them into a Conference Schedule
+ *
+ * @author Edwin Martinez
+ */
+public class Main {
+
+    public static void main(String[] args) {
+
+        List<Talk> talkList;
+
+        ConferenceUtil util = new ConferenceUtil();
+
+        try {
+            talkList = util.readInput(ConferenceConfig.TALKS_FILE);
+        } catch (FileNotFoundException | ConferenceOriginException e) {
+            System.err.println(e.getMessage());
+            return;
+        }
+
+        if(talkList.isEmpty()) {
+            System.err.println("Input File doesn't have any talk. Please check the input information.");
+        } else {
+            ConferenceManagement cm = new ConferenceManagement();
+            try {
+                cm.manageConference();
+            } catch (NullPointerException e) {
+                e.printStackTrace();
+            }
+        }
+
+    }
+}
